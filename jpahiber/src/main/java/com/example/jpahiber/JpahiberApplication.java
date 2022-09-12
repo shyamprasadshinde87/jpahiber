3723b75cfa54d0133de5cb09fbbfd345f8738c12
ShyamPrasad <shyamprasadshinde87@gmail.com>
2022-09-12 12:51:52 +0530

    adedd comment in application file

--- jpahiber/src/main/java/com/example/jpahiber/JpahiberApplication.java
+++ jpahiber/src/main/java/com/example/jpahiber/JpahiberApplication.java
@@ -3,6 +3,8 @@ package com.example.jpahiber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//Adding comment
 @SpringBootApplication
 public class JpahiberApplication {
 
