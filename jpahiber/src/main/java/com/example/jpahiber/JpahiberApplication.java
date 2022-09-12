e53c4ffaf8ea4055f6180b66aa2f7837bdc27e23
ShyamPrasad <shyamprasadshinde87@gmail.com>
2022-09-12 12:40:05 +0530

    Initial commit

---
+++ jpahiber/src/main/java/com/example/jpahiber/JpahiberApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpahiber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpahiberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpahiberApplication.class, args);
+	}
+
+}
